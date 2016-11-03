4794aa8106dc402a93f5ce2470d309bceb5aa22c
AhmadF <fanaei.ahmad@gmail.com>
2016-11-03 07:04:24 +0330

    add onOpen and onClose events

--- components/equipmentDrawer.js
+++ components/equipmentDrawer.js
@@ -10,12 +10,19 @@ class EqDrawer{
     this.element.addEventListener('hide.bs.modal',(e)=>{
       this.beforeColse();
     });
+    this.subIds=[];
+    this.vars=[];
   }
+  // we should add this events to the bootstrap.native project manually.
   afterOpen(){
     for(var i=0;i<this.vars.length;i++){
-      this.vars[i].subscribe(this.subIds[i],(newValue)=>{
-        this.element.querySelector(`#detail-${this.vars[i].sName}`).innerHTML=Math.round(newValue*100)/100;
-      });
+      let elem = this.element.querySelector(`#detail-${this.vars[i].sName}`);
+      this.subIds[i] = this.vars[i].subscribe(function(elem,newValue){
+         elem.querySelector(`.var-value`).innerHTML=Math.round(newValue*100)/100;
+      }.bind(this,elem));
+      elem.addEventListener('click',function(variable){
+        this.showChart(variable)
+      }.bind(this,this.vars[i]));
     }
   }
   beforeColse(){
@@ -25,14 +32,17 @@ class EqDrawer{
     this.vars=[];
     this.subIds=[];
   }
+  showChart(variable){
+    console.log(`${variable.name} is drawing`);
+  }
   draw(eq){
     let TAG = {main:`
       <div class='row'>
         <div class='col-xs-6 var-table'>
           <div class='row'>
             <div class='col-xs-5'>variable</div>
-            <div class='col-xs-3'>unit</div>
-            <div class='col-xs-4'>value</div>
+            <div class='col-xs-3 text-center'>unit</div>
+            <div class='col-xs-4 text-center'>value</div>
           </div>
           {$content}
         </div>
@@ -42,12 +52,12 @@ class EqDrawer{
     `
     };
     let content='';
-    this.vars = eq.nodesToMonitor[i];
+    this.vars = eq.nodesToMonitor;
     for(var i=0;i<eq.nodesToMonitor.length;i++){
-      content += `<div class='row'>
+      content += `<div class='row' id='detail-${eq.nodesToMonitor[i].sName}'>
             <div class='col-xs-5'>${eq.nodesToMonitor[i].name}</div>
-            <div class='col-xs-3'></div>
-            <div class='col-xs-4' id='detail-${eq.nodesToMonitor[i].sName}'>0</div>
+            <div class='col-xs-3 text-center'>${eq.nodesToMonitor[i].options.unit}</div>
+            <div class='col-xs-4 text-center' class='var-value'>0</div>
           </div>
           `;
     }
